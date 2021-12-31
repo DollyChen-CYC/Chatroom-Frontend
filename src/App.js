1bfe057b5095b1d2cc8dbbd4522229fbee029e54
DollyChen_CYC <dolly726033@gmail.com>
2021-12-31 20:59:21 +0800

    feat: create React-Redux

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
+import store from "../src/store"
+import { Provider } from "react-redux"
 import Navbar from './components/Navbar'
 import PublicChatroom from '../src/pages/PublicChatroom'
 import PrivateMessages from './pages/PrivateMessages'
@@ -8,20 +10,22 @@ import Register from './pages/Register'
 
 const App = () => {
   return (
-    <div className='App'>
-      <Navbar />
-      <main>
-        <Routes>
-          <Route path="/" element={<PublicChatroom />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/public" element={<PublicChatroom />} />
-          <Route path="/message/" element={<PrivateMessages />} />
-          <Route path="/message/:id" element={<PrivateMessages />} />
-          <Route path="*" element={<h1> 404 Page Not Found.</h1>} />
-        </Routes>
-      </main>
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<PublicChatroom />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/public" element={<PublicChatroom />} />
+            <Route path="/message/" element={<PrivateMessages />} />
+            <Route path="/message/:id" element={<PrivateMessages />} />
+            <Route path="*" element={<h1> 404 Page Not Found.</h1>} />
+          </Routes>
+        </main>
+      </div>
+    </Provider>
   )
 }
 
