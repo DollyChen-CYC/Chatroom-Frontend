563b667c26b123bad3aa824aefb9bc435f60962e
david3335 <sh330035@gmail.com>
2022-01-02 11:22:12 +0800

    feat.  list users card style setting

--- src/components/OnlineUserList.js
+++ src/components/OnlineUserList.js
@@ -10,68 +10,68 @@ const OnlineUserList = () => {
       <div className="list__users">
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple1</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">apple1</h3>
+          <h3 className="account font__gray__15__500">@apple</h3>
         </div>
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple2</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">Apple Orange</h3>
+          <h3 className="account font__gray__15__500">@apple1335</h3>
         </div>
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">Apple Banana</h3>
+          <h3 className="account font__gray__15__500">@Bpple1335</h3>
         </div>
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">Apple Orange</h3>
+          <h3 className="account font__gray__15__500">@apple1335</h3>
         </div>
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">Apple Banana</h3>
+          <h3 className="account font__gray__15__500">@Bpple1335</h3>
         </div>
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">Apple Orange</h3>
+          <h3 className="account font__gray__15__500">@apple1335</h3>
         </div>
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">Apple Banana</h3>
+          <h3 className="account font__gray__15__500">@Bpple1335</h3>
         </div>
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">Apple Orange</h3>
+          <h3 className="account font__gray__15__500">@apple1335</h3>
         </div>
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">Apple Banana</h3>
+          <h3 className="account font__gray__15__500">@Bpple1335</h3>
         </div>
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">Apple Orange</h3>
+          <h3 className="account font__gray__15__500">@apple1335</h3>
         </div>
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">Apple Banana</h3>
+          <h3 className="account font__gray__15__500">@Bpple1335</h3>
         </div>
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">Apple Orange</h3>
+          <h3 className="account font__gray__15__500">@apple1335</h3>
         </div>
         <div className="list__users__card">
           <div className="avatar"></div>
-          <div className="name">apple</div>
-          <div className="account">@apple</div>
+          <h3 className="name font__black__15__700">Apple Banana</h3>
+          <h3 className="account font__gray__15__500">@Bpple1335</h3>
         </div>
       </div>
     </div>
